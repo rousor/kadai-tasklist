fb3996a580bb5d7d5cdf32159f09cdf14c81b253
rousor <vw5yymst@gmail.com>
2018-10-01 08:41:37 +0000

    rerereport-kadai-tasklist

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,7 +5,7 @@
     <h1>タスク新規作成ページ</h1>
     
     <div class="row">
-        <div class="col-xs-12 col-md-offset-2  col-md-8  col-lg-offset-3　col-lg-6">
+        <div class="col-xs-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
 
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
@@ -24,4 +24,3 @@
     </div>
 
 @endsection
-
