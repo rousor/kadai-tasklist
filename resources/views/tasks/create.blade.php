c51046e299591a9817e48c489640fbd4c6ff1a47
rousor <vw5yymst@gmail.com>
2018-10-01 07:29:50 +0000

    rereport kadai-task

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,17 +5,19 @@
     <h1>タスク新規作成ページ</h1>
     
     <div class="row">
-        <div class="col-xs-12 col-md-8 col-md-offset-2 col-lg-6 col-md-offset-3">
+        <div class="col-xs-12 col-md-offset-2  col-md-8  col-lg-offset-3　col-lg-6">
 
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
+            <div class="form-group">
                 {!! Form::label('status', 'ステータス:') !!}
-                {!! Form::text('status') !!}
-
+                {!! Form::text('status', null, ['class' => 'form-control']) !!}
+            </div>
+            <div class="form-group">
                 {!! Form::label('content', 'タスク:') !!}
-                {!! Form::text('content') !!}
-
-                {!! Form::submit('投稿') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
+                {!! Form::submit('投稿', ['class' => 'btn btn-info']) !!}
 
             {!! Form::close() !!}
         </div>
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,17 +5,19 @@
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
     <div class="row">
-        <div class="col-xs-12 col-md-8 col-md-offset-2 col-lg-6 col-md-offset-3">
+        <div class="col-xs-12 col-md-offset-2  col-md-8  col-lg-offset-3　col-lg-6">
             
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-        
+
+            <div class="form-group">
                 {!! Form::label('status', 'ステータス:') !!}
-                {!! Form::text('status') !!}
-        
+                {!! Form::text('status', null, ['class' => 'form-control']) !!}
+            </div>
+            <div class="form-group">
                 {!! Form::label('content', 'タスク:') !!}
-                {!! Form::text('content') !!}
-        
-                {!! Form::submit('更新') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
+                {!! Form::submit('更新', ['class' => 'btn btn-info']) !!}
         
             {!! Form::close() !!}
         </div>
